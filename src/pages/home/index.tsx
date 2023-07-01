476396d44b89160d83e83e421f24dd44b73260ca
cheng yang <chengyang9966@gmail.com>
2023-07-01 20:14:37 +0800

    🐛 add timeout

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import { Typography ,Row, Col, message} from 'antd';
+import { Typography, Row, Col, message } from 'antd';
 import React, { useState, useEffect, Fragment } from 'react';
 import LoadingSpinner from '../../components/LoadingSpinner';
 import MainLayout from '../../layout/MainLayout';
@@ -7,7 +7,7 @@ import { submitFeedbackForm } from '../../services/feedback/feedback.service';
 import { useQuery } from '../../hooks/useQueryHooks/useQueryHook';
 
 const { Title } = Typography;
-const HomePage:React.FC = () => {
+const HomePage: React.FC = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [isSubmitFormLoading, setSubmitFormLoading] = useState(false);
   const [openFeedBackForm, setOpenFeedBackForm] = useState(true);
@@ -15,55 +15,56 @@ const HomePage:React.FC = () => {
   const [messageApi, contextHolder] = message.useMessage();
   const query = useQuery();
 
-//   useEffect(() => {
-//     const timer = setTimeout(() => {
-//       setIsLoading(false);
-//     }, 3000);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
 
-//     return () => clearTimeout(timer);
-//   }, []);
+    return () => clearTimeout(timer);
+  }, []);
 
-
-const submitForm=async(feedbackId:string)=>{
+  const submitForm = async (feedbackId: string) => {
     try {
-        setSubmitFormLoading(true)
-       const result= await submitFeedbackForm({
-            email:query.get('email'),
-            feedbackId,
-            remarks:''
-        })
-        messageApi.open({
-            type: 'success',
-            content: 'Submit Feedback Successfully',
-          });
-          setSubmitFormLoading(false)
-          setOpenFeedBackForm(false)
-
-
+      setSubmitFormLoading(true);
+      await submitFeedbackForm({
+        email: query.get('email'),
+        feedbackId,
+        remarks: ''
+      });
+      messageApi.open({
+        type: 'success',
+        content: 'Submit Feedback Successfully'
+      });
+      setSubmitFormLoading(false);
+      setOpenFeedBackForm(false);
     } catch (error) {
-        messageApi.open({
-            type: 'error',
-            content: 'Fail to Submit Feedback',
-          });
-          setSubmitFormLoading(false)
+      messageApi.open({
+        type: 'error',
+        content: 'Fail to Submit Feedback'
+      });
+      setSubmitFormLoading(false);
     }
-  }
+  };
 
   return (
     <Fragment>
-    {contextHolder}
-        <Row style={{textAlign:'center'}}>
+      {contextHolder}
+      <Row style={{ textAlign: 'center' }}>
         <Col span={24}>
-      {isLoading  || isSubmitFormLoading? (
-        <LoadingSpinner />
-      ) : (
-        <MainLayout>
-        <Title level={2}>Congratz! You have completed the class</Title>
-        <FeedBackModel submitForm={submitForm} handleCancelFeedBackForm={()=>setOpenFeedBackForm(false)} openFeedBackForm={openFeedBackForm}/>
-        </MainLayout>
-      )}
-      </Col>
-        </Row>
+          {isLoading || isSubmitFormLoading ? (
+            <LoadingSpinner />
+          ) : (
+            <MainLayout>
+              <Title level={2}>Congratz! You have completed the class</Title>
+              <FeedBackModel
+                submitForm={submitForm}
+                handleCancelFeedBackForm={() => setOpenFeedBackForm(false)}
+                openFeedBackForm={openFeedBackForm}
+              />
+            </MainLayout>
+          )}
+        </Col>
+      </Row>
     </Fragment>
   );
 };
